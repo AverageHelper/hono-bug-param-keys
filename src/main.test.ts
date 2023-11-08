3acf8ccf8585499bb7dd9c13190ce4cea91f6009
AverageHelper <1500092+AverageHelper@users.noreply.github.com>
2023-11-08 10:02:06 -0700

    chore: Remove extra components

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,32 +1,10 @@
-import { version as packageVersion } from "./version.js";
-
-const mockParseArgs = vi.fn();
-vi.mock("node:util", () => ({ parseArgs: mockParseArgs }));
-
-const mockConsoleInfo = vi.spyOn(console, "info");
+import { describe, expect, test } from "vitest";
+import { app } from "./main.js";
 
 describe("Main", () => {
-	test("does something", async () => {
-		mockParseArgs.mockReturnValue({
-			values: {
-				version: false
-			}
-		});
-		await import("./main.js");
-
-		expect(mockParseArgs).toHaveBeenCalledOnce();
-		expect(mockConsoleInfo).toHaveBeenCalledExactlyOnceWith("TODO: Do something here!");
-	});
-
-	test("prints the package version", async () => {
-		mockParseArgs.mockReturnValue({
-			values: {
-				version: true
-			}
-		});
-		await import("./main.js");
-
-		expect(mockParseArgs).toHaveBeenCalledOnce();
-		expect(mockConsoleInfo).toHaveBeenCalledExactlyOnceWith(`v${packageVersion}`);
+	test.each(["baz", "bin", "keys"])("GET responds with '%s'", async p => {
+		const res = await app.request(`/api/v0/db/users/:uid/${p}/:documentId`);
+		expect(res.status).toBe(200);
+		expect(await res.json()).toMatchObject(["string", p]);
 	});
 });
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,10 @@
-import "source-map-support/register.js";
-import { parseArgs as _parseArgs } from "node:util";
-import { version as packageVersion } from "./version.js";
+import { Hono } from "hono";
 
-const { values } = _parseArgs({
-	options: {
-		// Show the version, then exit
-		version: { short: "v", type: "boolean", default: false }
-	},
-	strict: true
+const route = new Hono().get("/users/:uid/:collectionId/:documentId", c => {
+	const p = c.req.param("collectionId");
+	return c.json([typeof p, p.toString()]);
 });
 
-if (values.version) {
-	// eslint-disable-next-line no-console
-	console.info(`v${packageVersion}`);
-} else {
-	// eslint-disable-next-line no-console
-	console.info("TODO: Do something here!");
-}
-
-// TODO: This is untestable. Reorganize parseArgs to a separate file; Change this test to check we call args; Test parseArgs that it returns the version flag when given.
+export const app = new Hono() //
+	.basePath("/api")
+	.route("/v0/db", route);
